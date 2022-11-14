45056418552c4d5e6a591e6d22747f9b90bbc218
ellen_hein <elle7227@stud.kea.dk>
2022-11-14 11:20:17 +0100

    json

--- wp-content/themes/norrebro_united_theme/page-u9mix.php
+++ wp-content/themes/norrebro_united_theme/page-u9mix.php
@@ -21,25 +21,25 @@ get_header();
 	</main><!-- #main -->
 <script>
 	const siteUrl = "https://helenaadelaide.dk/kea/09_cms/norrebrounited/wp-json/wp/v2/u9mix/";
-	let kampe_håndbold =[];
-	const kamp_håndboldTemp = document.querySelector("#holder");
-	const kamp_håndboldListe = document.querySelector("#template");
+	let kampe_handbold =[];
+	const kamp_handboldTemp = document.querySelector("#holder");
+	const kamp_handboldListe = document.querySelector("#template");
 	getJson();
 	async function getJson(){
 		const response = await fetch(siteUrl);
-		kampe_håndbold = await response.json();
-		console.log(kampe_håndbold);
-		vis(kampe_håndbold);
+		kampe_handbold = await response.json();
+		console.log(kampe_handbold);
+		vis(kampe_handbold);
 	}
 
 	function vis(json) {
         const holder = document.querySelector("#holder");
         const skabelon = document.querySelector("template");
 
-        json.forEach((kamp_håndbold) => {
-          console.log("kamp_håndbold",kamp_håndbold);
+        json.forEach((kamp_handbold) => {
+          console.log("kamp_handbold",kamp_håndbold);
           const klon = skabelon.cloneNode(true).content;
-          klon.querySelector("h3").textContent = kamp_håndbold.lokation;
+          klon.querySelector("h3").textContent = kamp_handbold.lokation;
           holder.appendChild(klon);
         });
       }
